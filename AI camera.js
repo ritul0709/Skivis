27f8863b458b75672f255a884ae94d72ccfaca57
ritul0709 <ritulmahaveeragency1979@gmail.com>
2026-02-07 12:10:36 +0530

    Add initial AI camera functionality

---
+++ AI camera.js
@@ -0,0 +1,7 @@
+const MODEL_URL = "https://teachablemachine.withgoogle.com/models/I7v0XUu4-/"; 
+
+let model, webcam, labelContainer, maxPredictions;
+
+async function init() { /* SAME CODE */ }
+async function loop() { /* SAME CODE */ }
+async function predict() { /* SAME CODE */ }
